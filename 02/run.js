61bae80feca7f9f38e3701e014fef10f39ce4616
Romain Perez <romain.perez@check24.de>
2020-12-02 09:09:19 +0100

    day 2

---
+++ 02/run.js
@@ -0,0 +1,40 @@
+module.exports = (input) => {
+    const start = process.hrtime();
+    const lines = input.split('\n');
+
+    let validCount1 = 0;
+    let validCount2 = 0;
+    lines.forEach((line) => {
+        line = line.trim();
+        if (!line.length) {
+            return;
+        }
+
+        const dashPosition = line.indexOf('-');
+        const periodPosition = line.indexOf(':');
+        const min = parseInt(line.substring(0, dashPosition), 10);
+        const max = parseInt(line.substring(dashPosition + 1, periodPosition - 2), 10);
+        const character = line.substring(periodPosition - 1, periodPosition);
+        const password = line.substring(periodPosition + 2);
+
+        let occurrences = 0;
+        for (let i = 0; i < password.length; i++) {
+            if (password[i] === character) {
+                occurrences++;
+            }
+        }
+
+        if (occurrences >= min && occurrences <= max) {
+            validCount1++;
+        }
+
+        if (password[min - 1] !== password[max - 1] && (password[min - 1] === character || password[max - 1] === character)) {
+            validCount2++;
+        }
+    });
+
+    console.log('Part 1: ', validCount1);
+    console.log('Part 2: ', validCount2);
+    let time = process.hrtime(start);
+    console.log('Time: ', (1000 * time[0] + Math.floor(time[1] / 1000000)) + 'ms');
+};
